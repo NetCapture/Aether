179b44210677202cfea6bac9343b8490a12bbe5f
miqt <miqingtang@163.com>
2020-07-28 11:41:11 +0800

    url decode

--- app/src/main/java/cn/demo/appq/utils/CustomizeDecoder.java
+++ app/src/main/java/cn/demo/appq/utils/CustomizeDecoder.java
@@ -1,11 +1,18 @@
 package cn.demo.appq.utils;
 
+import com.blankj.utilcode.util.EncodeUtils;
+
 import cn.demo.appq.entity.ReqEntity;
 
 public class CustomizeDecoder implements IDecoder {
     @Override
     public ReqEntity decode(ReqEntity entity) {
-        // TODO: 2020/7/23 自定义实现
+        try {
+            entity.setReqContent(EncodeUtils.urlDecode(entity.getReqContent()));
+            entity.setRespContent(EncodeUtils.urlDecode(entity.getRespContent()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return entity;
     }
 }
